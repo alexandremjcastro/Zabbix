7167b2586af92bbd48266bf9439e49cb7a30edea
Alexandre Castro <alexandremjcastro@outlook.com>
2019-11-07 17:18:24 -0300

    Update sefaznfe.consulta.sh

--- Monitoramento Sefaz/sefaznfe.consulta.sh
+++ Monitoramento Sefaz/sefaznfe.consulta.sh
@@ -1,17 +1,42 @@
-#!/bin/bash
-# SCRIPT CONSULTA O STATUS DO SERVICO DE NFE DIRETAMENTE DA RECEITA FEDERAL
-# SCRIPT FEITO BASEADO NO AUTORIZADOR CONSULTA_AUTORIZADOR
-
-# VARIAVEIS
-
+#!/usr/bin/env bash
+#
+# sefaznfe.consulta.sh - Consulta o status do serviço de NFE da receita federal
+#
+# GitHub:       https://github.com/alexandremjcastro/
+# Autor:        Alexandre Castro
+# Manutenção: -
+#
+# ------------------------------------------------------------------------ #
+#  - Este programa irá consultar o status do serviço de NFE da receita federal
+#  Responsável por realizar a consulta dentro do arquivo statusNFE.txt,
+#  nas consultas será pesquisada as linhas do Autorizador e cada linha de serviço
+#  deste autorizador, nesse script ele verificará as alterações das “Bolinhas”
+#  em cada serviço, se for verde (Disponível) ele apresentará o resultado 1, 2
+#  para amarelo (Indisponível) e 0 para Vermelho(Offine).
+#
+#  - Script criado com base no script do @bernardolankheet.
+#
+#   Exemplos:
+#      $ ./sefaznfe.consulta.sh AM AUTORIZACAO
+#      Neste exemplo o script realiza a consulta do campo de "Autorização" do
+#      estado de AM. Retornando o valor 1, 2 ou 0.
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 07/11/2019, Alexandre:
+#     - Script criado.
+#
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   bash 4.2.46
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
 CURL=$(which curl)
 AWK=$(which awk)
 CAT=$(which cat)
 ESTADO=$1
 STATUS=$2
-
-ARQUIVO_TEMPORARIO="/tmp/statusNFE.txt"
-
+# ------------------------------- FUNÇÕES ----------------------------------------- #
+# Status dos estados
 function consultar_servico() {
         [[ $STATUS_AUTORIZACAO == "imagens/bola_verde_P.png" ]] && echo "1" #DISPONIVEL
         [[ $STATUS_AUTORIZACAO == "imagens/bola_amarela_P.png" ]] && echo "2" #INDISPONIVEL
@@ -47,7 +72,11 @@ function consultar_servico() {
         [[ $STATUS_RECEPCAO_EVENTO3 == "imagens/bola_amarela_P.png" ]] && echo "2" #INDISPONIVEL
         [[ $STATUS_RECEPCAO_EVENTO3 == "imagens/bola_vermelho_P.png" ]] && echo "0" #OFFLINE
 }
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
+# Arquivo com as informações da página.
+ARQUIVO_TEMPORARIO="/tmp/statusNFE.txt"
 
+# Verificar o status dos estados
 if [[ $ESTADO,$STATUS == @(AM|BA|CE|GO|MG|MS|MT|PE|PR|RS|SP|SVAN|SVRS|SVC-AN|SVC-RS),AUTORIZACAO ]]; then
   STATUS_AUTORIZACAO=$($CAT $ARQUIVO_TEMPORARIO | egrep "<td>$ESTADO</td>" | $AWK '{print $2}' | $AWK -F 'src="' '{print $2}'| $AWK -F '"' '{print $1}') && consultar_servico
     elif [[ $ESTADO,$STATUS == @(AM|BA|CE|GO|MG|MS|MT|PE|PR|RS|SP|SVAN|SVRS|SVC-AN|SVC-RS),RETORNO.AUT ]]; then
